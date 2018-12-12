3a87139da8dc0f555e0b615ce40d7a2d56ec9765
woonzh <zhenhao1991@gmail.com>
2018-12-12 23:53:07 +0800

    t

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import requests
 url='https://woonzh.herokuapp.com/lazprice'
 
 params={
-    'product': 'wood storage cupboard'
+    'product': 'wood storage cupboards'
         }
 
 jid=requests.get(url, params=params)
