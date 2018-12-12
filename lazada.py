3feb70f08691606a96f49d9072cd5be6ef60ed74
woonzh <zhenhao1991@gmail.com>
2018-12-13 00:12:09 +0800

    t

--- lazada.py
+++ lazada.py
@@ -80,7 +80,7 @@ def getProduct(name):
     options.binary_location = chromebin
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    options.add_argument('headless')
+#    options.add_argument('headless')
     driver = webdriver.Chrome(executable_path=chromepath, chrome_options=options)
     driver.maximize_window()
     mainURL="https://www.lazada.sg"
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import requests
 url='https://woonzh.herokuapp.com/lazprice'
 
 params={
-    'product': 'containers'
+    'product': 'plastic containers'
         }
 
 jid=requests.get(url, params=params)
