55db68b67e2f4c34245b1a1bdca07cf59610f353
woonzh <zhenhao1991@gmail.com>
2018-12-16 12:50:25 +0800

    t

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import requests
 url='https://woonzh.herokuapp.com/lazprice'
 
 params={
-    'product': 'addidas slippers'
+    'product': 'nightlamp'
         }
 
 jid=requests.get(url, params=params)
