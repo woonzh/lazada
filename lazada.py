8f6f87a90c8276cf48df0310808551dcb264e5c6
woonzh <zhenhao1991@gmail.com>
2018-12-16 11:45:04 +0800

    t

--- lazada.py
+++ lazada.py
@@ -83,8 +83,8 @@ def getProduct(name):
     options=Options()
     options.binary_location = GOOGLE_CHROME_BIN
     options.add_argument('--disable-gpu')
-#    options.add_argument('--no-sandbox')
-    options.add_argument('headless')
+    options.add_argument('--no-sandbox')
+#    options.add_argument('headless')
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
     driver.maximize_window()
     mainURL="https://www.lazada.sg"
