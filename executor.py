89c6ea2050e77c0ff9d7128b0d93762a512e9b79
woonzh <zhenhao1991@gmail.com>
2018-12-18 00:09:34 +0800

    t

---
+++ executor.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 17 22:06:05 2018
+
+@author: ASUS
+"""
+
+import pandas as pd
+from lazada import lazadaCrawl
+
+lazCrawler = lazadaCrawl()
+
--- lazada.py
+++ lazada.py
@@ -9,133 +9,146 @@ import time
 import pandas as pd
 import os
 
-xpaths={
-    'name':'.//div[@class="c16H9d"]', 
-    'price':'.//span[@class="c13VH6"]', 
-    'orgPrice':'.//del[@class="c13VH6"]', 
-    'discount':'.//span[@class="c1hkC1"]',
-    'reviews': './/span[@class="c3XbGJ"]',
-    'country': './/span[@class="c2i43- "]'}
+class lazadaCrawl:    
+    def __init__(self, server=False, container = False, windows=True):
+        self.xpaths={
+            'name':'.//div[@class="c16H9d"]', 
+            'price':'.//span[@class="c13VH6"]', 
+            'orgPrice':'.//del[@class="c13VH6"]', 
+            'discount':'.//span[@class="c1hkC1"]',
+            'reviews': './/span[@class="c3XbGJ"]',
+            'country': './/span[@class="c2i43- "]'}
+        
+        if server:
+            t=1
+        else:
+            if container:
+                self.chromepath='./chromedriver(linux)/chromedriver'
+            
+                self.options=webdriver.ChromeOptions()
+                self.options.add_argument('--headless')
+                self.options.add_argument('--no-sandbox')
+                self.options.add_argument("--disable-setuid-sandbox")
+                self.driver = webdriver.Chrome(chrome_options=self.options)
+            else:
+                if windows:
+                    self.chromepath='chromedriver/chromedriver.exe'
+                else:
+                    self.chromepath='chromedriver(linux)/chromedriver'
+                    
+                self.options=webdriver.ChromeOptions()
+#                self.options.add_argument('--headless')
+                self.driver = webdriver.Chrome(self.chromepath, chrome_options=self.options)                    
+            
+    def close(self):
+        self.driver.quit()
 
-def parseData(info, attribute='innerText'):
-    lst=[]
-    for i in info:
-        lst.append(i.get_attribute(attribute))
-    
-    return lst
-
-def getText(element, xpath):
-#    print(xpath)
-    try:
-        txt=element.find_element_by_xpath(xpath).text
-    except:
-        txt=''
-    return txt
-
-def parseMain(info):
-    df=pd.DataFrame(columns=['name', 'price', 'orginal price', 'discount', 'reviews', 'country'])
-    count=0
-    for i in info:
-#        print(i.text)
+    def parseData(self, info, attribute='innerText'):
         lst=[]
-        for j in xpaths:
-            lst.append(getText(i, xpaths[j]))
-        df.loc[count]=lst
-        count+=1
-    return df
-
-def nonServerGetProduct(name):
-    print('test')
-    chromepath='./chromedriver(linux)/chromedriver'
+        for i in info:
+            lst.append(i.get_attribute(attribute))
+        
+        return lst
     
-    options=webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-setuid-sandbox")
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.maximize_window()
-    mainURL="https://www.lazada.sg"
-    driver.get(mainURL)
+    def getText(self, element, xpath):
+        try:
+            txt=element.find_element_by_xpath(xpath).text
+        except:
+            txt=''
+        return txt
     
-    time.sleep(5)
+    def parseMain(self, info):
+        df=pd.DataFrame(columns=['name', 'price', 'orginal price', 'discount', 'reviews', 'country'])
+        count=0
+        for i in info:
+    #        print(i.text)
+            lst=[]
+            for j in self.xpaths:
+                lst.append(self.getText(i, self.xpaths[j]))
+            df.loc[count]=lst
+            count+=1
+        return df
     
-    first=driver.page_source
+    def nonServerGetProduct(self,name):
+        self.driver.maximize_window()
+        mainURL="https://www.lazada.sg"
+        self.driver.get(mainURL)
+        
+        time.sleep(5)
+        
+#        first=self.driver.page_source
+        
+        inForm=self.driver.find_element_by_id('q')
+        inForm.send_keys(name)
+        
+        button=self.driver.find_element_by_class_name('search-box__button--1oH7')
+#        print(button)
+        self.driver.execute_script("arguments[0].click();", button)
+        
+        time.sleep(4)
+        
+        mains=self.driver.find_elements_by_xpath('//div[@class="c3KeDq"]')
+        
+        df=self.parseMain(mains)
+        
+#        a={'first':first,
+#           'second': self.driver.page_source}
+        return df
     
-    inForm=driver.find_element_by_id('q')
-    inForm.send_keys(name)
+    def getProduct(self,name):
+    #    chromebin='/app/.apt/usr/bin/google-chrome'
+    #    chromepath='/app/.chromedriver/bin/chromedriver' 
+        GOOGLE_CHROME_BIN=os.getenv('GOOGLE_CHROME_BIN')
+        CHROMEDRIVER_PATH=os.getenv('CHROMEDRIVER_PATH')
+        
+        print('google chrome bin: %s'%(GOOGLE_CHROME_BIN))
+        print('chromdriver: %s'%(CHROMEDRIVER_PATH))
+        
+        options=Options()
+        options.binary_location = GOOGLE_CHROME_BIN
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+    #    options.add_argument("user-data-dir=selenium") 
+    #    options.add_argument('headless')
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+        driver.maximize_window()
+        mainURL="https://www.lazada.sg"
+    #    mainURL="https://blog.codeship.com/get-selenium-to-wait-for-page-load/"
+        driver.get(mainURL)
+        
+        print(name)
+        
+        time.sleep(5)
+        
+        first=driver.page_source
+        
+        inForm=driver.find_element_by_id('q')
+        inForm.send_keys(name)
+        
+        logo=driver.find_elements_by_class_name('lzd-logo-content')
+        print('blogo %s'%(logo))
+        
+        driver.find_element_by_class_name('search-box__button--1oH7').click()
+    ##    
+        time.sleep(3)
+    ##    
+        logo=driver.find_elements_by_class_name('lzd-logo-content')
+        print('blogo2 %s'%(logo))
+    #    
+        mains=driver.find_elements_by_xpath('//div[@class="c3KeDq"]')
+        print(mains)
+        df=self.parseMain(mains)
+        print(len(df))
+        a={'first':first,
+           'second': driver.page_source}  
     
-    button=driver.find_element_by_class_name('search-box__button--1oH7')
-    print(button)
-    driver.execute_script("arguments[0].click();", button)
-#    
-    time.sleep(4)
-#    
-    mains=driver.find_elements_by_xpath('//div[@class="c3KeDq"]')
+        time.sleep(1)
+        
+        driver.quit()
+        
+    #    df=pd.DataFrame(columns=['name', 'price', 'orginal price', 'discount', 'reviews', 'country'])
+        return a
     
-    df=parseMain(mains)
-    
-    a=driver.page_source
-    
-    a={'first':first,
-       'second': driver.page_source}
-    
-    driver.quit()
-    
-
-    return df
-
-#df=nonServerGetProduct('nike tshirt')
-
-def getProduct(name):
-#    chromebin='/app/.apt/usr/bin/google-chrome'
-#    chromepath='/app/.chromedriver/bin/chromedriver' 
-    GOOGLE_CHROME_BIN=os.getenv('GOOGLE_CHROME_BIN')
-    CHROMEDRIVER_PATH=os.getenv('CHROMEDRIVER_PATH')
-    
-    print('google chrome bin: %s'%(GOOGLE_CHROME_BIN))
-    print('chromdriver: %s'%(CHROMEDRIVER_PATH))
-    
-    options=Options()
-    options.binary_location = GOOGLE_CHROME_BIN
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-#    options.add_argument("user-data-dir=selenium") 
-#    options.add_argument('headless')
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
-    driver.maximize_window()
-    mainURL="https://www.lazada.sg"
-#    mainURL="https://blog.codeship.com/get-selenium-to-wait-for-page-load/"
-    driver.get(mainURL)
-    
-    print(name)
-    
-    time.sleep(5)
-    
-    first=driver.page_source
-    
-    inForm=driver.find_element_by_id('q')
-    inForm.send_keys(name)
-    
-    logo=driver.find_elements_by_class_name('lzd-logo-content')
-    print('blogo %s'%(logo))
-    
-    driver.find_element_by_class_name('search-box__button--1oH7').click()
-##    
-    time.sleep(3)
-##    
-    logo=driver.find_elements_by_class_name('lzd-logo-content')
-    print('blogo2 %s'%(logo))
-#    
-    mains=driver.find_elements_by_xpath('//div[@class="c3KeDq"]')
-    print(mains)
-    df=parseMain(mains)
-    print(len(df))
-    a={'first':first,
-       'second': driver.page_source}  
-
-    time.sleep(1)
-    
-    driver.quit()
-    
-#    df=pd.DataFrame(columns=['name', 'price', 'orginal price', 'discount', 'reviews', 'country'])
-    return a
+#laz=lazadaCrawl()
+#df=laz.nonServerGetProduct('nalgen water bottle')
+#laz.close()
