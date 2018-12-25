f2a05ed79036aaa52a935de6215fadd311d3eb5b
woonzh <zhenhao1991@gmail.com>
2018-12-25 12:02:16 +0800

    deployed website for product scrape

--- static/js/prodSearch.js
+++ static/js/prodSearch.js
@@ -76,17 +76,66 @@ function productSearch(){
   url="http://localhost:8080/product";
   product=document.getElementById("product").value;
 
-  $.ajax({
-    url: url,
-    type: 'GET',
-    data:{product: product},
-    success: function (data) {
-      displayResults(data);
-      document.getElementById("loading").style.display="none";
-    },
-    error: function(jqxhr, status, exception) {
-      alert("Error.");
-      document.getElementById("loading").style.display="none";
+  if (product == ""){
+    alert("Please fill in the product name before pressing search.")
+  }else{
+    $.ajax({
+      url: url,
+      type: 'GET',
+      data:{product: product},
+      success: function (data) {
+        displayResults(data);
+        document.getElementById("loading").style.display="none";
+      },
+      error: function(jqxhr, status, exception) {
+        alert("Error.");
+        document.getElementById("loading").style.display="none";
+      }
+    });
+  }
+}
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+function exportTableToCSV() {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+
+    for (var i = 0; i < rows.length; i++) {
+      if (i!=1){
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+
+        for (var j = 0; j < cols.length; j++)
+            row.push(cols[j].innerText);
+
+        csv.push(row.join(","));
+      }
     }
-  });
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), "products.csv");
 }
